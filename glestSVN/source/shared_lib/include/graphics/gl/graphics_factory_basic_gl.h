8498cd563d7a2b7a5a73382d6dfe2bdf6c3eb55d
wangtianhang <690879430@qq.com>
2016-05-01 19:17:43 +0800

    补提交

--- glestSVN/source/shared_lib/include/graphics/gl/graphics_factory_basic_gl.h
+++ glestSVN/source/shared_lib/include/graphics/gl/graphics_factory_basic_gl.h
@@ -26,17 +26,17 @@ namespace Shared{ namespace Graphics{ namespace Gl{
 //	class GraphicsFactoryBasicGl
 // =====================================================
 
-class GraphicsFactoryBasicGl: public GraphicsFactory{
-public:
-	virtual TextRenderer2D *newTextRenderer2D()	{return new TextRenderer2DGl();}
-	virtual TextRenderer3D *newTextRenderer3D()	{return new TextRenderer3DGl();}
-	virtual ModelRenderer *newModelRenderer()	{return new ModelRendererGl();}
-	virtual Context *newContext()				{return new ContextGl();}
-	virtual Model *newModel()					{return new ModelGl();}
-	virtual Texture2D *newTexture2D()			{return new Texture2DGl();}
-	virtual Font2D *newFont2D()					{return new Font2DGl();}
-	virtual Font3D *newFont3D()					{return new Font3DGl();}
-};
+// class GraphicsFactoryBasicGl: public GraphicsFactory{
+// public:
+// 	virtual TextRenderer2D *newTextRenderer2D()	{return new TextRenderer2DGl();}
+// 	virtual TextRenderer3D *newTextRenderer3D()	{return new TextRenderer3DGl();}
+// 	virtual ModelRenderer *newModelRenderer()	{return new ModelRendererGl();}
+// 	virtual Context *newContext()				{return new ContextGl();}
+// 	virtual Model *newModel()					{return new ModelGl();}
+// 	virtual Texture2D *newTexture2D()			{return new Texture2DGl();}
+// 	virtual Font2D *newFont2D()					{return new Font2DGl();}
+// 	virtual Font3D *newFont3D()					{return new Font3DGl();}
+// };
 
 }}}//end namespace
 
