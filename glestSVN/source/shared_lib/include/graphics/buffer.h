2563a2c2a26237d70f6c6c8aab04d276ef61e45c
wangtianhang <690879430@qq.com>
2016-05-01 15:42:08 +0800

    干掉buffer

--- glestSVN/source/shared_lib/include/graphics/buffer.h
+++ glestSVN/source/shared_lib/include/graphics/buffer.h
@@ -11,50 +11,50 @@ namespace Shared{ namespace Graphics{
 //	class VertexBuffer
 // =====================================================
 
-class VertexBuffer{
-private:
-	static const int texCoordCount = 8;
-	static const int attribCount = 8;
-
-private:
-	void *positionPointer;
-	void *normalPointer;
-
-	void *texCoordPointers[texCoordCount];
-	int texCoordCoordCounts[texCoordCount];
-
-	void *attribPointers[attribCount];
-	int attribCoordCounts[attribCount];
-	string attribNames[attribCount];
-
-public:
-	VertexBuffer();
-	virtual ~VertexBuffer(){};
-
-	virtual void init(int size)= 0;
-
-	void setPositionPointer(void *pointer);
-	void setNormalPointer(void *pointer);
-	void setTexCoordPointer(void *pointer, int texCoordIndex, int coordCount);
-	void setAttribPointer(void *pointer, int attribIndex, int coordCount, const string &name);
-};
+// class VertexBuffer{
+// private:
+// 	static const int texCoordCount = 8;
+// 	static const int attribCount = 8;
+// 
+// private:
+// 	void *positionPointer;
+// 	void *normalPointer;
+// 
+// 	void *texCoordPointers[texCoordCount];
+// 	int texCoordCoordCounts[texCoordCount];
+// 
+// 	void *attribPointers[attribCount];
+// 	int attribCoordCounts[attribCount];
+// 	string attribNames[attribCount];
+// 
+// public:
+// 	VertexBuffer();
+// 	virtual ~VertexBuffer(){};
+// 
+// 	virtual void init(int size)= 0;
+// 
+// 	void setPositionPointer(void *pointer);
+// 	void setNormalPointer(void *pointer);
+// 	void setTexCoordPointer(void *pointer, int texCoordIndex, int coordCount);
+// 	void setAttribPointer(void *pointer, int attribIndex, int coordCount, const string &name);
+// };
 
 // =====================================================
 //	class IndexBuffer
 // =====================================================
 
-class IndexBuffer{
-private:
-	void *indexPointer;
-
-public:
-	IndexBuffer();
-	virtual ~IndexBuffer(){}
-
-	virtual void init(int size)= 0;
-
-	void setIndexPointer(void *pointer);
-};
+// class IndexBuffer{
+// private:
+// 	void *indexPointer;
+// 
+// public:
+// 	IndexBuffer();
+// 	virtual ~IndexBuffer(){}
+// 
+// 	virtual void init(int size)= 0;
+// 
+// 	void setIndexPointer(void *pointer);
+// };
 
 }}//end namespace
 
